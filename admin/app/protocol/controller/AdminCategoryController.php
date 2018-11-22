c55cb17a2549c4da181ab811b44e035158627730
zhenghl <zhenghl@t-chip.com.cn>
2018-11-22 19:41:11 +0800

    update

--- admin/app/protocol/controller/AdminCategoryController.php
+++ admin/app/protocol/controller/AdminCategoryController.php
@@ -176,7 +176,7 @@ class AdminCategoryController extends AdminBaseController
         }
 
         if($data['mode_type']){
-            $data['more']['axes'] = $this->mode_type[$data['mode_type']];
+            // $data['more']['axes'] = $this->mode_type[$data['mode_type']];
         }
         // dump($data);
         $result = $protocolCategoryModel->addCategory($data);
