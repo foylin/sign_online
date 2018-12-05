471a96e1d5bad0341db99b6dc4f66b7a16841851
unocus <464471063@qq.com>
2018-12-05 16:40:26 +0800

    mini1205

--- admin/data/runtime/cache/1a/64788d489a00f94589600c5d122b0e.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-s:93:"/var/www/sign_online/admin/public/../data/runtime/cache/c3/0b008a754ac7f1eeef085057ee3106.php";
---
+++ miniprogram/pages/sign/sign2.js
@@ -0,0 +1,80 @@
+
+var api     = require('../../utils/api.js');
+var app     = getApp();
+
+Page({
+  
+    // 视图数据
+    data: {
+      selectColor: 'black'
+    },
+
+    onLoad(params) {
+      this.params = params;
+      console.log(this.params)
+      // return;
+      if (params.post_id) {
+        // wx.showNavigationBarLoading();
+        wx.showLoading({title: '印章生成中...'})
+        api.get({
+            url: 'protocol/lists/sec_through?post_id=' + params.post_id + '&pcup_id=' + params.pcup_id,
+            data: {},
+            success: data => {
+                // wx.hideNavigationBarLoading();
+                wx.hideLoading()
+                if (data.code == 0) {
+                  wx.showModal({
+                    content: data.msg,
+                    showCancel: false,
+                    success: function (res) {
+                        if (res.confirm) {
+                            console.log('用户点击确定')
+                            
+                        }
+                    }
+                  })
+                }else{
+
+                  wx.showToast({
+                    title: '审批成功',
+                    icon: 'none',
+                    duration: 1000,
+                    
+                  })
+
+                    setTimeout(function(){
+                        // wx.navigateBack({
+                        //     delta: 1
+                        // })
+                        wx.switchTab({
+                            url: '/pages/index/index'
+                        });
+                    }, 1000)
+
+                    
+                    
+                }
+            },
+            fail: err => {
+                // wx.hideNavigationBarLoading();
+                wx.hideLoading()
+                wx.showToast({
+                  title: '审批失败',
+                  icon: 'none',
+                  duration: 1000,
+                  
+                })
+
+                setTimeout(function(){
+                    // wx.navigateBack({
+                    //     delta: 1
+                    // })
+                    wx.navigateTo({
+                        url: '/pages/index/index'
+                    });
+                }, 1000)
+            }
+        });
+    }
+    }
+  })
